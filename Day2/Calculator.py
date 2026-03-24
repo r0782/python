90dfc47a5740c5ba4ec84809851099511a3fdccd
raghunandan <raghunandan@raghunandans-MacBook-Pro.local>
2026-03-24 12:03:40 +0530

    DAY 2 tasks

---
+++ Day2/Calculator.py
@@ -0,0 +1,15 @@
+Number1=float(input("Enter a number:"))
+Operator=input("Entre a operation which you want to do:")
+Number2=float(input("Enter a number:"))
+if Operator == "+":
+    print("Answer is :",Number1+Number2)
+elif Operator == "-":
+     print("Answer is :",Number1-Number2)
+elif Operator == "*":
+     print("Answer is :",Number1*Number2)
+elif Operator == "%":
+     print("Answer is :",Number1%Number2)
+elif Operator == "/":
+     print("Answer is :",Number1/Number2)
+elif Operator == "**":
+     print("Answer is :",Number1**Number2)
